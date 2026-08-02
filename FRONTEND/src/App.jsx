4cece49d2023f11235eedbdc73de99f8f1a1ded9
Naaz Uppal <naazuppal@Naazs-MacBook-Pro.local>
2026-08-03 02:12:11 +0530

    Initial commit: Full Stack URL Shortener

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+import LoginForm from "./components/LoginForm.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+
+const App = () => {
+  return (
+    <>
+      <AuthPage />
+    </>
+  );
+};
+export default App;
